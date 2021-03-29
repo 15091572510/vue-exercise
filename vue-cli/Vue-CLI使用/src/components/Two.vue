924c75cdef6f28519b8ffd427df0bd5436e26552
wangweifeng <seven_wwf@foxmail.com>
2021-03-29 22:05:53 +0800

    init

---
+++ vue-cli/Vue-CLI使用/src/components/Two.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>我是第二个子组件</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Two'
+}
+</script>
+<!-- 注意点：默认情况下组件中的style设置的样式是全局的样式 -->
+<style scoped>
+
+</style>
---
+++ vue-cli/vue-plugin/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <Loading></Loading> -->
+    <button @click="myFn1">显示</button>
+    <button @click="myFn2">隐藏</button>
+  </div>
+</template>
+<script>
+  // import Loading from './components/Loading'
+  // import Vue from 'vue'
+  export default {
+    // components: {
+      // Loading
+    // }
+    methods: {
+      myFn1 () {
+        // Vue.showLoading()
+        this.$showLoading()
+      },
+      myFn2 () {
+        // Vue.hideLoading()
+        this.$hideLoading()
+      }
+    }
+  }
+</script>
+<style lang="scss">
+</style>
